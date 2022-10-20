39f17484c16607fda1b6658861fd25217a70280e
agaktr <katradis.agamemnon@gmail.com>
2022-10-20 19:23:53 +0300

    a

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,6 +18,7 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
